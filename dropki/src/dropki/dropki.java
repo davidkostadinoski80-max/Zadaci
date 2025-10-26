cefe18246584f5ee1b494e277acf1fe05a996370
davidkostadinoski80-max <davidkostadinoski80@gmail.com>
2025-10-26 08:54:39 -0500

    0

---
+++ dropki/src/dropki/dropki.java
@@ -0,0 +1,40 @@
+package dropki;
+
+import java.util.Scanner;
+
+public class dropki {
+	
+	public static void main (String[] args) {
+	
+		int n1,n2,n1prim,n2prim;
+		Scanner tastatura=new Scanner(System.in);
+		System.out.print("Vnesete broitel: ");
+		n1=tastatura.nextInt();
+		
+		System.out.print("Vnesete imenitel: ");
+		n2=tastatura.nextInt();
+		
+		n1prim=n1;
+		n2prim=n2;
+		
+		while (n1!=n2){
+			if (n1>n2) {
+				n1=n1-n2;
+				
+			}else {
+				n2=n2-n1;
+			}
+			
+			}
+		int nzd=n1;
+		System.out.print("\n Nzd na broevite e:"+nzd);
+		System.out.print("\n Skratenata dropka e:"+n1prim/nzd+"/"+n2prim/nzd);
+	}
+	
+		
+		}
+		
+		
+	
+
+
